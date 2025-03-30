6109211133e8f7cad98a376b6dda9a4b89d0a5c8
dhruvpujara <pujaradhruv373@gmail.com>
2025-03-30 21:03:11 +0530

    Update index2.js

--- JAVASCRIPT/index2.js
+++ JAVASCRIPT/index2.js
@@ -17,6 +17,7 @@ bar.addEventListener("click",() =>{
 animes2.forEach((anime2) => {
     let div = document.createElement("div");
     div.classList.add("ANIME"); 
+    div.classList.add("ANIME2"); 
     div.classList.add("border");
     content.append(div);
 
@@ -26,8 +27,6 @@ animes2.forEach((anime2) => {
     aniimgDiv.classList.add("aniimg");
     aniimgDiv.style.backgroundImage = `url('${anime2.Image}')`;
  
-    let aniimg2 = document.querySelectorAll(".aniimg");
-    let index = 1;
  
     // play button
     let wtbtn = document.createElement("button");
@@ -38,7 +37,6 @@ animes2.forEach((anime2) => {
    
     wtbtn.addEventListener("click", () => {
       const animedet = `/anime-det2.ejs/${anime2.Id}`;
-      console.log("hi");
       console.log(animedet);
       document.location.href = animedet;
     });
